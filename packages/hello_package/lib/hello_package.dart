7f716152abb70d647e2949e0eaa8bb17a69ac1d4
nirmalatgallabox <129173419+nirmalatgallabox@users.noreply.github.com>
2024-04-23 12:51:00 +0530

    first commit

---
+++ packages/hello_package/lib/hello_package.dart
@@ -0,0 +1,12 @@
+library hello_package;
+
+import 'package:flutter/widgets.dart';
+
+class HelloPackage extends StatelessWidget {
+  const HelloPackage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello Package');
+  }
+}
---
+++ packages/zen_package/lib/zen_package.dart
@@ -0,0 +1,13 @@
+library zen_package;
+
+import 'package:flutter/material.dart';
+
+/// A Calculator.
+class ZenPackage extends StatelessWidget {
+  const ZenPackage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Zen Package v0.0.1');
+  }
+}
