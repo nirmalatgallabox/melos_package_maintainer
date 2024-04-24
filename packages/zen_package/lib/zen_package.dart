56ffb77c273ae517259f3ccd5a4c4b49973537e5
nirmalatgallabox <129173419+nirmalatgallabox@users.noreply.github.com>
2024-04-24 15:06:41 +0530

    feat(zen_package): style change
    Breaking Change: added new styling

--- packages/zen_package/lib/zen_package.dart
+++ packages/zen_package/lib/zen_package.dart
@@ -11,8 +11,8 @@ class ZenPackage extends StatelessWidget {
     return const Text(
       'Zen Package v0.0.11',
       style: TextStyle(
-        color: Colors.yellow,
-        fontSize: 13,
+        color: Colors.green,
+        fontSize: 14,
       ),
     );
   }
